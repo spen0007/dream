707c6b1d85c70046c941e2f6e759c0bd3faade6b
spen0007 <spen0007@users.noreply.github.com>
2021-07-04 21:04:26 +0100

    Update stage6.py

--- stage6.py
+++ stage6.py
@@ -1,18 +1,17 @@
 #new function to create a jpg from a byte array
 def savejpg(a, filename, fmt='jpeg'):
     a = np.uint8(np.clip(a, 0, 255))
-    f = BytesIO()
     PIL.Image.fromarray(a).save(filename, fmt)
 
 # the code will start here on stage 6
 # we need some file handling    
-import os, re, os.path
+import os, re, os.path, zipfile
 # create the outputs folder if it does not exist
 if not os.path.exists('outputs'):
     os.makedirs('outputs')
 
-#clean up any existing output files
-for root, dirs, files in os.walk("outputs"):
+# clean up any existing output files
+for root, dirs, files in os.walk('outputs'):
     for file in files:
         os.remove(os.path.join(root, file))
 
@@ -28,12 +27,25 @@ img_y, img_x, _ = img0.shape
 for i in range(zooming_steps):
   frame = render_deepdream(tf.square(T(layer)), frame, False)
   clear_output()
-  filename="outputs/dreamstage" + str(i) +".jpg"
   showarray(frame)
+  
   #save each iteration in the outputs folder
+  filename="outputs/dreamstage" + str(i) +".jpg"
   savejpg(frame, filename)
+
+
   newsize = np.int32(np.float32(frame.shape[:2])*zoom_factor)
   frame = resize(frame, newsize)
   frame = frame[(newsize[0]-img_y)//2:(newsize[0]-img_y)//2+img_y,
                 (newsize[1]-img_x)//2:(newsize[1]-img_x)//2+img_x,:]
-  
+
+
+# zip up all the images in outputs and put it in the root
+zipDreamstages = zipfile.ZipFile('dreams.zip', 'w')
+for root, dirs, files in os.walk('outputs'):
+  for file in files:
+      zipDreamstages.write(os.path.join(root, file),file)
+zipDreamstages.close()
+
+
+
